7adae5caee7d34c9d1854277aab7c5f4b495b852
Richard Virgosky <119339307+RichardVirgosky@users.noreply.github.com>
2024-11-14 00:51:22 +0100

    New Release v0.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d261a47b-606a-4802-947f-ffc66a972473")]
-[assembly: AssemblyVersion("0.1")]
-[assembly: AssemblyFileVersion("0.1")]
+[assembly: AssemblyVersion("0.2")]
+[assembly: AssemblyFileVersion("0.2")]
